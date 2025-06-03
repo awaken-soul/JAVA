418fe5be877e39a8e9da381542fc178a822d1b2e
ALAN JOY <alanjoy812006@gmail.com>
2025-06-03 10:31:11 +0530

    Update Main.java

--- Note/Java_Language_Basics/Main.java
+++ Note/Java_Language_Basics/Main.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+class Main{
+  public static void main(String[] args){
+    String month = "January";
+    int month_num = -1;
+    switch (month.toLowerCase()){
+      case "january": 
+        month_num = 1;
+        break;
+    }
+  }
+} 
+
+Scanner scanner = new scanner.(System.in);
+int quater = scanner.nextLine();
+
+public String connvertquatertoseason(int quater){
+      String quater_label = switch(quater){
+                              case 0 -> {
+                                System.out.println("Winter");
+                                  yeild "winter";
+                              
+                              }
+                            }
+}
